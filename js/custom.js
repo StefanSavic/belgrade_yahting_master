07313c8196745d33e61e762650e420e2722f476e
StefanSavic <stevjah11@gmail.com>
2020-06-11 10:27:36 +0200

    fix instagram feed plugin, add new version of plugin and add new access token via fb developers page

--- js/custom.js
+++ js/custom.js
@@ -9,33 +9,45 @@
 // });
 // userFeed.run();
 // console.log('ello')
+var feed = new Instafeed({
+	limit:12,
+	accessToken: 'IGQVJVM09KX3VkMVI5VVlVTEpLN253N3hmay15c3duTE1lYVZAxSml4b1hCdGgzdjJFODJIVHlRTjlVbmlzSnMtX3JFOHlkWDZABekc3TmljUnpodk1FY29WYW5FZATlnRVcyRm9rd19sb1hrdUpQWnNuWgZDZD',
+	// target:'instafeed',
+	debug: true,
+	template: '<div class="flex image-wrapper col-md-3 col-sm-12 col-xs-12"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>'
+});
+feed.run();
 var d = new Date();
 var n = d.getFullYear();
 document.getElementById("currentYear").innerHTML = n;
-var Instagramfeed = [];
-fetch("https://www.instagram.com/belgradeyachting/?__a=1") // Call the fetch function passing the url of the API as a parameter
-	.then((resp) => resp.json()) // Transform the data into json
-	.then(function (data) {
-		data.graphql.user.edge_owner_to_timeline_media.edges.forEach(image => {
-			this.Instagramfeed.push(image.node.display_url)
-		});
+// var Instagramfeed = [];
+// fetch("https://www.instagram.com/belgradeyachting/?__a=1") // Call the fetch function passing the url of the API as a parameter
+// 	.then((resp) => resp.json()) // Transform the data into json
+// 	.then((data)=>{
+// 		console.log(data);
+// 	})  // Transform the data into json
+// 	.then(function (data) {
+// 		data.graphql.user.edge_owner_to_timeline_media.edges.forEach(image => {
+// 			this.Instagramfeed.push(image.node.display_url)
+// 		});
 
-		console.log(data.graphql);
-		console.log(this.Instagramfeed);
-		// Create and append the li's to the ul
-	})
-	.catch(function () {
-		// This is where you run code if the server returns any errors
-	});
+// 		console.log(data.graphql);
+// 		console.log(this.Instagramfeed);
+// 		// Create and append the li's to the ul
+// 	})
+// 	.catch(function (data) {
+// 		console.log(data)
+// 		// This is where you run code if the server returns any errors
+// 	});
 
-setInterval(() => {
-	var str = '<div class="row">'
-	Instagramfeed.forEach(function (image) {
+// setTimeout(() => {
+// 	var str = '<div class="row">'
+// 	Instagramfeed.forEach(function (image) {
 		
-		// str += `<img src="${image}"/>`
-		str += `<div class="flex image-wrapper col-md-3 col-sm-6 col-xs-12"><a target='_blank' href="https://www.instagram.com/belgradeyachting/"><img src="${image}" alt="{{caption}}" class="img-fluid"/></a></div>`
-	});
+// 		// str += `<img src="${image}"/>`
+// 		str += `<div class="flex image-wrapper col-md-3 col-sm-6 col-xs-12"><a target='_blank' href="https://www.instagram.com/belgradeyachting/"><img src="${image}" alt="{{caption}}" class="img-fluid"/></a></div>`
+// 	});
 
-	str += '</div>';
-	document.getElementById("instafeed").innerHTML = str;
-}, 700);
+// 	str += '</div>';
+// 	document.getElementById("instafeed").innerHTML = str;
+// }, 1000);
